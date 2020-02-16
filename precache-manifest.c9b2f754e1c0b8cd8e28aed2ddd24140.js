f37f842384358c9e53e36a11102bf5b5e71a4332
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-02-16 12:20:12 +0100

    Updates

---
+++ precache-manifest.c9b2f754e1c0b8cd8e28aed2ddd24140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9b5cee4556ae9e4a5f2aa14b34c66f",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "15c173aa2876ded00c84",
+    "url": "/movies-app/static/css/main.05b25528.chunk.css"
+  },
+  {
+    "revision": "cac2263d14c0f05949fe",
+    "url": "/movies-app/static/js/2.9973ea22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movies-app/static/js/2.9973ea22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c173aa2876ded00c84",
+    "url": "/movies-app/static/js/main.1834abd7.chunk.js"
+  },
+  {
+    "revision": "e53c414ce6b9d6f1a8db",
+    "url": "/movies-app/static/js/runtime-main.1a5337ea.js"
+  }
+]);
